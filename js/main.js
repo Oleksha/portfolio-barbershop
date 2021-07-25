babcb08ae7930b92c60e59a26093f4137234d686
Oleksha <deryabino@gmail.com>
2021-07-25 15:05:55 +0400

    Оживил модальное окно Логин

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// JavaScript Document
+var link = document.querySelector(".login-link");
+var popup = document.querySelector(".modal-login");
+var close = document.querySelector(".modal-close");
+var login = popup.querySelector("[name=login]");
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  login.focus();
+});
+
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
