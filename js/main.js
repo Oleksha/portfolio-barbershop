487fe5bfcf33deb83e9bc9a58149088896c5c3c1
Oleksha <deryabino@gmail.com>
2021-07-25 16:49:51 +0400

    Оформил основное поведение формы Логин

--- js/main.js
+++ js/main.js
@@ -2,14 +2,41 @@ var link = document.querySelector(".login-link");
 var popup = document.querySelector(".modal-login");
 var close = document.querySelector(".modal-close");
 var login = popup.querySelector("[name=login]");
+var password = popup.querySelector("[name=password]");
+var form = popup.querySelector("form");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
-  login.focus();
+  popup.classList.add("modal-show");  
+  if (storage) { 
+    login.value = storage; 
+    password.focus();
+  } else {
+    login.focus();
+  }
 });
 
 close.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) { 
+  if (!login.value || !password.value) { 
+    evt.preventDefault(); 
+    console.log("Нужно ввести логин и пароль"); 
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }    
+  }
 });
